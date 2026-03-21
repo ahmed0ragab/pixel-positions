5e9facd5f021b33b71cd366e8f1f50e332cf1206
ahmed0ragab <ahmedabdelmotaleb1999@gmail.com>
2026-03-21 03:49:51 -0800

    feat:creation of models relationships on models level

---
+++ database/migrations/2026_03_21_110217_create_jobs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employer_id')->constrained('employers')->cascadeOnDelete();
+            $table->string('title');
+            $table->string('salary');
+            $table->string('location');
+            $table->enum('time_of_work',['fulltime','parttime']);
+            $table->string('url');//url to company site which contain all details about the job
+            $table->text('description');
+            $table->boolean('featured')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
